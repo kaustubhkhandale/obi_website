6b6ca969ddeebcaca34920efdfc42b8d3691d6d9
Kaustubhkhandale <kaustubh.khandale@orangebitsindia.com>
2024-01-08 16:34:49 +0530

    team section update

--- team_section.php
+++ team_section.php
@@ -1,126 +1,230 @@
  <!--====== INFETECH TEAM PART ENDS ======-->
 
  <section class="infetech-team-area">
-      <div class="container">
-        <div class="row align-items-center mb-50">
-          <div class="col-lg-6">
-            <div class="section-title">
-              <span>Our Expert People</span>
-              <h4 class="title">Meet Our Professional Team Members</h4>
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <div class="section-title pl-100">
-              <p>
-                There are many variations of passages of available but majority
-                have suffered alteration in some form, by humou or randomised
-                words which don't look even slightly believable.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-4 col-md-6">
-            <div
-              class="single-tema-item animated wow fadeInUp"
-              data-wow-duration="1500ms"
-              data-wow-delay="0ms"
-            >
-              <div class="top-line"></div>
-              <div class="thumb">
-                <img src="assets/images/team-1.jpg" alt="" />
-              </div>
-              <div class="content">
-                <h4 class="title">Christine Eve</h4>
-                <span>Web Developer</span>
-                <div class="share-icon">
-                  <i class="fas fa-share-alt"></i>
-                  <ul>
-                    <li>
-                      <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-twitter"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-instagram"></i></a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6">
-            <div
-              class="single-tema-item animated wow fadeInUp"
-              data-wow-duration="1500ms"
-              data-wow-delay="300ms"
-            >
-              <div class="top-line"></div>
-              <div class="thumb">
-                <img src="assets/images/team-2.jpg" alt="" />
-              </div>
-              <div class="content">
-                <h4 class="title">Mike Hardson</h4>
-                <span>UI/UX Designer</span>
-                <div class="share-icon">
-                  <i class="fas fa-share-alt"></i>
-                  <ul>
-                    <li>
-                      <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-twitter"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-instagram"></i></a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6">
-            <div
-              class="single-tema-item animated wow fadeInUp"
-              data-wow-duration="1500ms"
-              data-wow-delay="600ms"
-            >
-              <div class="top-line"></div>
-              <div class="thumb">
-                <img src="assets/images/team-3.jpg" alt="" />
-              </div>
-              <div class="content">
-                <h4 class="title">Jessica Brown</h4>
-                <span>Web Designer</span>
-                <div class="share-icon">
-                  <i class="fas fa-share-alt"></i>
-                  <ul>
-                    <li>
-                      <a href="#"><i class="fab fa-facebook-f"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-twitter"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                    </li>
-                    <li>
-                      <a href="#"><i class="fab fa-instagram"></i></a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+     <div class="container">
+         <div class="row align-items-center mb-50">
+             <div class="col-lg-6">
+                 <div class="section-title">
+                     <span>Our Expert People</span>
+                     <h4 class="title">Meet Our Professional Team Members</h4>
+                 </div>
+             </div>
+             <div class="col-lg-6">
+                 <div class="section-title pl-100">
+                     <p>
+                         There are many variations of passages of available but majority
+                         have suffered alteration in some form, by humou or randomised
+                         words which don't look even slightly believable.
+                     </p>
+                 </div>
+             </div>
+         </div>
+         <div class="row">
+             <div class="col-lg-4 col-md-6 mb-3">
+                 <div class="single-tema-item animated wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="0ms">
+                     <div class="top-line"></div>
+                     <div class="thumb">
+                         <img src="assets/images/team-1.jpg" alt="" />
+                     </div>
+                     <div class="content">
+                         <h4 class="title">Rajesh Watkar</h4>
+                         <span>Founder and CEO</span>
+                         <div class="share-icon">
+                             <i class="fas fa-share-alt"></i>
+                             <ul>
+                                 <li>
+                                     <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-twitter"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-instagram"></i></a>
+                                 </li>
+                             </ul>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-4 col-md-6 mb-3">
+                 <div class="single-tema-item animated wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="300ms">
+                     <div class="top-line"></div>
+                     <div class="thumb">
+                         <img src="assets/images/team-2.jpg" alt="" />
+                     </div>
+                     <div class="content">
+                         <h4 class="title">Parag Deote</h4>
+                         <span>Founder and COO</span>
+                         <div class="share-icon">
+                             <i class="fas fa-share-alt"></i>
+                             <ul>
+                                 <li>
+                                     <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-twitter"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-instagram"></i></a>
+                                 </li>
+                             </ul>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-4 col-md-6 mb-3">
+                 <div class="single-tema-item animated wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="600ms">
+                     <div class="top-line"></div>
+                     <div class="thumb">
+                         <img src="assets/images/team-3.jpg" alt="" />
+                     </div>
+                     <div class="content">
+                         <h4 class="title">Sachin Mahajan</h4>
+                         <span>Co-Founder and CTO</span>
+                         <div class="share-icon">
+                             <i class="fas fa-share-alt"></i>
+                             <ul>
+                                 <li>
+                                     <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-twitter"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-instagram"></i></a>
+                                 </li>
+                             </ul>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-4 col-md-6 mb-3">
+                 <div class="single-tema-item animated wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="600ms">
+                     <div class="top-line"></div>
+                     <div class="thumb">
+                         <img src="assets/images/team-3.jpg" alt="" />
+                     </div>
+                     <div class="content">
+                         <h4 class="title">Parag Thakur</h4>
+                         <span>Chief Sales Officer</span>
+                         <div class="share-icon">
+                             <i class="fas fa-share-alt"></i>
+                             <ul>
+                                 <li>
+                                     <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-twitter"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-instagram"></i></a>
+                                 </li>
+                             </ul>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-4 col-md-6 mb-3">
+                 <div class="single-tema-item animated wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="600ms">
+                     <div class="top-line"></div>
+                     <div class="thumb">
+                         <img src="assets/images/team-3.jpg" alt="" />
+                     </div>
+                     <div class="content">
+                         <h4 class="title">Ruchi Jain</h4>
+                         <span>Associate Director HR</span>
+                         <div class="share-icon">
+                             <i class="fas fa-share-alt"></i>
+                             <ul>
+                                 <li>
+                                     <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-twitter"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-instagram"></i></a>
+                                 </li>
+                             </ul>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-4 col-md-6 mb-3">
+                 <div class="single-tema-item animated wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="600ms">
+                     <div class="top-line"></div>
+                     <div class="thumb">
+                         <img src="assets/images/team-3.jpg" alt="" />
+                     </div>
+                     <div class="content">
+                         <h4 class="title">Sumit Gupta</h4>
+                         <span>VP Sales</span>
+                         <div class="share-icon">
+                             <i class="fas fa-share-alt"></i>
+                             <ul>
+                                 <li>
+                                     <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-twitter"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-instagram"></i></a>
+                                 </li>
+                             </ul>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+             <div class="col-lg-4 col-md-6 mb-3">
+                 <div class="single-tema-item animated wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="600ms">
+                     <div class="top-line"></div>
+                     <div class="thumb">
+                         <img src="assets/images/team-3.jpg" alt="" />
+                     </div>
+                     <div class="content">
+                         <h4 class="title">Rachana Merai</h4>
+                         <span>VP Sales</span>
+                         <div class="share-icon">
+                             <i class="fas fa-share-alt"></i>
+                             <ul>
+                                 <li>
+                                     <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-twitter"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                                 </li>
+                                 <li>
+                                     <a href="#"><i class="fab fa-instagram"></i></a>
+                                 </li>
+                             </ul>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
 
-    <!--====== INFETECH TEAM PART ENDS ======-->
+ <!--====== INFETECH TEAM PART ENDS ======-->
